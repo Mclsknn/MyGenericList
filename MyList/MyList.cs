29f7131f9025184e4b51cbbe6025dd21b7317a48
Mclsknn <46161661+Mclsknn@users.noreply.github.com>
2021-03-02 15:24:13 +0300

    Add project files.

---
+++ MyList/MyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+   public class MyList<TKey,TValue>
+    {
+        TKey[] _keyArray;
+        TValue[] _valueArray;
+        TKey[] _tempKeyArray;
+        TValue[] _tempValueArray;
+        public MyList()
+        {
+            _keyArray = new TKey[0];
+            _valueArray = new TValue[0];
+        }
+        public void Add(TKey key,TValue value) {
+            _tempKeyArray = _keyArray;
+            _tempValueArray = _valueArray;
+            _keyArray = new TKey[_keyArray.Length + 1];
+            _valueArray = new TValue[_valueArray.Length + 1];
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = _tempKeyArray[i];
+            }
+            _keyArray[_keyArray.Length - 1] = key;
+            for (int i = 0; i < _tempValueArray.Length; i++)
+            {
+                _valueArray[i] = _tempValueArray[i];
+            }
+            _valueArray[_valueArray.Length - 1] = value;
+        }
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<int, string> dizi = new MyList<int, string>();
+            dizi.Add(12, "Mustafa");
+            dizi.Add(13, "Mustafa");
+            dizi.Add(14, "Mustafa");
+            dizi.Add(15, "Mustafa");
+
+        }
+    }
+}
